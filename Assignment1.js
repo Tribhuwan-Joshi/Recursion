6995d43cc41bd29fca391a6a705d6e363650079f
Tribhuwan <mathincosmos@gmail.com>
2022-09-27 13:32:53 +0530

    add Fibonacci recursive

--- Assignment1.js
+++ Assignment1.js
@@ -8,3 +8,14 @@ function fibs(n) {
     return arr;
 }
 
+function fibsRec(n) {
+    let arr = [0, 1];
+    if (n <= 0) return []
+    if (n <= 2) return arr.slice(0, n);
+    let res = fibsRec(n - 1);
+    res.push(res[res.length - 1] + res[res.length - 2]);
+    return res
+    
+}
+
+console.log(fibsRec(3))  // [0,1,1,2]
